233087138ed6a0976a338859f3f4e46e5c7d5020
N2O <837673608@qq.com>
2022-05-23 16:16:00 +0800

    dhps_counter

--- shared/net_tables.d.ts
+++ shared/net_tables.d.ts
@@ -9,12 +9,10 @@ declare interface CustomNetTableDeclarations {
     hero_list: {
         hero_list: Record<string, string> | string[];
     };
-    custom_net_table_1: {
-        key_1: number;
-        key_2: string;
-    };
-    custom_net_table_3: {
-        key_1: number;
-        key_2: string;
+    DHPS: {
+        DHPS:{
+            DPS: number;
+            HPS: number;
+        }  
     };
 }
